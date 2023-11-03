0ac835cb2c9a72d129a442db1b11b19c864b26cf
AlgoZenithNITC <147307721+AlgoZenithNITC@users.noreply.github.com>
2023-11-03 07:06:06 +0530

    Create Pythagorean_Triplet.cpp

---
+++ Pythagorean_Triplet.cpp
@@ -0,0 +1,31 @@
+class Solution{
+public:
+	bool checkTriplet(int arr[], int n){
+	    int maximum = *max_element(arr, arr + n);
+	    vector<bool> squares(maximum + 1, false);
+	    for(int i = 0; i < n; i++){
+	        squares[arr[i]] = true;
+	    }
+	    for(int i = 1; i <= maximum; i++){
+	        if(squares[i] == false){
+	            continue;
+	        }
+	        for(int j = 1; j <= maximum; j++){
+	            if(squares[j] == false or i == j){
+	                continue;
+	            }
+	            int height_square = i*i;
+	            int bredth_square = j*j;
+	            int hyp_square = height_square + bredth_square;
+	            int hyp = sqrt(hyp_square);
+	            if(hyp*hyp != hyp_square){
+	                continue;
+	            }
+	            if(squares[hyp] == true){
+	                return true;
+	            }
+	        }
+	    }
+	    return false;
+	}
+};
