1f453507966e83ae43e9bd27dc44af6ad91a496b
AlgoZenithNITC <147307721+AlgoZenithNITC@users.noreply.github.com>
2023-11-19 07:50:59 +0530

    Create Intersection_of_Two_linked_lists.java

---
+++ Intersection_of_Two_linked_lists.java
@@ -0,0 +1,22 @@
+class Solution{
+   public static Node findIntersection(Node head1, Node head2){
+    Node answer = new Node(0);
+    Node temp = answer;
+    while(head1 != null && head2 != null){
+        if(head1.data < head2.data){
+            head1 = head1.next;
+        }
+        else if(head1.data > head2.data){
+            head2 = head2.next;
+        }
+        else{
+            answer.next = head1;
+            answer = answer.next;
+            head1 = head1.next;
+            head2 = head2.next;
+        }
+    }
+    answer.next = null;
+    return temp.next;
+    }
+}
