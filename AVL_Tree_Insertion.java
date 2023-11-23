f7f42a66f5cccb4dd2a851f3810d773ecf1c9bf3
AlgoZenithNITC <147307721+AlgoZenithNITC@users.noreply.github.com>
2023-11-23 08:00:53 +0530

    Create AVL_Tree_Insertion.java

---
+++ AVL_Tree_Insertion.java
@@ -0,0 +1,61 @@
+class Solution{
+    int height(Node N) {
+        if (N == null)
+            return 0;
+        return N.height;
+    }
+
+    int getBalance(Node N) {
+        if (N == null)
+            return 0;
+        return height(N.left) - height(N.right);
+    }
+
+    Node rightRotate(Node y) {
+        Node x = y.left;
+        Node T2 = x.right;
+        x.right = y;
+        y.left = T2;
+        y.height = Math.max(height(y.left), height(y.right)) + 1;
+        x.height = Math.max(height(x.left), height(x.right)) + 1;
+        return x;
+    }
+
+    Node leftRotate(Node x) {
+        Node y = x.right;
+        Node T2 = y.left;
+        y.left = x;
+        x.right = T2;
+        x.height = Math.max(height(x.left), height(x.right)) + 1;
+        y.height = Math.max(height(y.left), height(y.right)) + 1;
+        return y;
+    }
+    
+    public  Node insertToAVL(Node node, int data){
+        if(node == null){
+            return new Node(data);
+        }
+        if(data < node.data){
+            node.left = insertToAVL(node.left, data);
+        }else if(data > node.data){
+            node.right = insertToAVL(node.right, data);
+        }else{
+            return node;
+        }
+        node.height = 1 + Math.max(height(node.left), height(node.right));
+        int balance = getBalance(node);
+        if (balance > 1 && data < node.left.data)
+            return rightRotate(node);
+        if (balance < -1 && data > node.right.data)
+            return leftRotate(node);
+        if (balance > 1 && data > node.left.data) {
+            node.left = leftRotate(node.left);
+            return rightRotate(node);
+        }
+        if (balance < -1 && data < node.right.data) {
+            node.right = rightRotate(node.right);
+            return leftRotate(node);
+        }
+        return node;
+    }
+}  
