3ba530ae387d2bc595c57ffde52361ea6b9a8ad2
AlgoZenithNITC <147307721+AlgoZenithNITC@users.noreply.github.com>
2023-12-18 06:01:01 +0530

    Create Game_Of_Xor.java

---
+++ Game_Of_Xor.java
@@ -0,0 +1,14 @@
+class Solution {
+    static int gameOfXor(int N , int[] A) {
+        if(N%2 == 0){
+            return 0;
+        }
+        int final_xor = 0;
+        for(int i = 0; i < N; i++){
+            if(i%2 == 0){
+                final_xor ^= A[i];
+            }
+        }
+        return final_xor;
+    }
+};
