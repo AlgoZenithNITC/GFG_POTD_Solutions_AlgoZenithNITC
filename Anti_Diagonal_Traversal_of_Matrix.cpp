0aa5a0c1c3f78f6776f701ece670a2c5d848fa5a
AlgoZenithNITC <147307721+AlgoZenithNITC@users.noreply.github.com>
2023-12-27 05:16:02 +0530

    Update Anti_Diagonal_Traversal_of_Matrix.cpp

--- Anti_Diagonal_Traversal_of_Matrix.cpp
+++ Anti_Diagonal_Traversal_of_Matrix.cpp
@@ -1,10 +1,18 @@
 class Solution {
   public:
     vector<int> antiDiagonalPattern(vector<vector<int>> matrix){
-        vector<vector<int>> result(matrix.size() + matrix[0].size());
-        for(int i = 0; i < matrix.size(); ++i){
-            for(int j = 0; j < matrix[0].size(); ++j){
-                result[i + j].push_back(matrix[i][j]);
+        vector<int> result;
+        int n = matrix.size();
+        for(int i = 0; i < n; i++){
+            int row = 0, col = i;
+            while(row < n and col >= 0){
+                result.push_back(matrix[row++][col--]);
+            }
+        }
+        for(int i = 1; i < n; i++){
+            int row = i, col = n - 1;
+            while(row < n and col >= 0){
+                result.push_back(matrix[row++][col--]);
             }
         }
         return result;
