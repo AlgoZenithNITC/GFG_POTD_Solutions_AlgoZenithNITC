d82561719c9ba74113bc0e9ebea7a6ecab4753e1
AlgoZenithNITC <147307721+AlgoZenithNITC@users.noreply.github.com>
2024-02-16 19:15:39 +0530

    Update 16_02_2024_Flatten_BST_to_sorted_list.java

--- 16_02_2024_Flatten_BST_to_sorted_list.java
+++ 16_02_2024_Flatten_BST_to_sorted_list.java
@@ -1,1 +1,34 @@
-http://16_02_2024_flatten_bst_to_sorted_list.java/
+class Pair{
+    Node head;
+    Node tail;
+    Pair(Node h,Node t){
+        head = h;
+        tail = t;
+    }
+}
+class Solution {
+    private Pair helper(Node root){
+        if(root==null)
+            return new Pair(null,null);
+        
+        Pair leftP = helper(root.left);
+        Pair rightP = helper(root.right);
+        
+        Pair ans = new Pair(root,root);
+        if(leftP.head!=null){
+            leftP.tail.right = root;
+            ans.head = leftP.head;
+        }
+        if(rightP.tail!=null){
+            root.right = rightP.head;
+            ans.tail = rightP.tail;
+        }
+        root.left=null;
+        return ans;
+    }
+    public Node flattenBST(Node root) {
+        // Code here
+        Pair ans = helper(root);
+        return ans.head;
+    }
+}
