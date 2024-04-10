9c804fdd0de48c8e8cbdd0a37182998d43c2d5bc
AlgoZenithNITC <147307721+AlgoZenithNITC@users.noreply.github.com>
2024-04-10 17:09:21 +0530

    Create 10-04-2024_Party_of_Couples.java

---
+++ 10-04-2024_Party_of_Couples.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int findSingle(int n, int[] arr) {
+        int ans = arr[0];
+        for (int i = 1; i < n; i++) {
+            ans = ans ^ arr[i];
+        }
+        return ans;
+    }
+}
+
