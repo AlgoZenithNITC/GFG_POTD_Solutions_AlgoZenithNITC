f73e8ea288d8529d5d0e2a98dbc735b2ea4bb117
AlgoZenithNITC <147307721+AlgoZenithNITC@users.noreply.github.com>
2024-05-19 19:08:07 +0530

    Create 19-05-2024_Find the closest number.py

---
+++ 19-05-2024_Find the closest number.py
@@ -0,0 +1,33 @@
+from typing import List
+
+
+class Solution:
+    def findClosest(self, n : int, k : int, arr : List[int]) -> int:
+        if k <= arr[0]:
+            return arr[0]
+        if k >= arr[n - 1]:
+            return arr[n - 1]
+
+        l, r = 0, n - 1
+
+        while l < r:
+            mid = (l + r) // 2
+            if arr[mid] == k:
+                return arr[mid]
+            if k < arr[mid]:
+                if mid > 0 and k > arr[mid - 1]:
+                    if (k - arr[mid - 1]) < (arr[mid] - k):
+                        return arr[mid - 1]
+                    else:
+                        return arr[mid]
+                r = mid
+            else:
+                if mid < n - 1 and k < arr[mid + 1]:
+                    if (k - arr[mid]) < (arr[mid + 1] - k):
+                        return arr[mid]
+                    else:
+                        return arr[mid + 1]
+                l = mid + 1
+
+        return arr[l]
+        
