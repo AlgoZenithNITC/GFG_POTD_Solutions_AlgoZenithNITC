3c7f6abe257474831482e1bd2aff0610a67a0661
AlgoZenithNITC <147307721+AlgoZenithNITC@users.noreply.github.com>
2024-06-14 13:20:10 +0530

    Create 14-06-2024_Armstrong_Numbers.cpp

---
+++ 14-06-2024_Armstrong_Numbers.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string armstrongNumber(int n) {
+        int originalNumber = n;
+        int sumOfCubes = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            sumOfCubes += digit * digit * digit;
+            n /= 10;
+        }
+
+        if (sumOfCubes == originalNumber) {
+            return "true";
+        } else {
+            return "false";
+        }
+    }
+};
