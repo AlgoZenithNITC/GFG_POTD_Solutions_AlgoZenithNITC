8abf9805fa228b94a2ba4fe1a79773be2ee4b580
AlgoZenithNITC <147307721+AlgoZenithNITC@users.noreply.github.com>
2024-06-20 16:45:16 +0530

    Create 20-06-2024_Integral_Points_Inside_Triangle..cpp

---
+++ 20-06-2024_Integral_Points_Inside_Triangle..cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+  
+    long long int gcd(long long int a, long long int b){
+        if(b == 0) return a;
+        return gcd(b, a%b);
+    }
+    
+    long long int getBoundaryCount(long long int p[], long long int q[]){
+        if(p[0] == q[0]) return abs(p[1] - q[1]) - 1;
+        if(p[1] == q[1]) return abs(p[0] - q[0]) - 1;
+        
+        return gcd(abs(p[0] - q[0]), abs(p[1] - q[1])) - 1;
+    }
+    
+    long long int InternalCount(long long int p[], long long int q[],long long int r[]) {
+        long long int boundaryPoints = getBoundaryCount(p, q) + getBoundaryCount(p, r) + getBoundaryCount(q, r) + 3;
+        long long int doubleArea = abs(p[0] * (q[1] - r[1]) + q[0] * (r[1] - p[1]) + r[0] * (p[1] - q[1]));
+        
+        return (doubleArea - boundaryPoints + 2)/2;
+    }
+};
