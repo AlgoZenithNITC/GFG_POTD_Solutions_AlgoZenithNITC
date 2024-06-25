da52c0485b2d9043572d139e33907be1ad679411
AlgoZenithNITC <147307721+AlgoZenithNITC@users.noreply.github.com>
2024-06-25 15:24:55 +0530

    Create 25_06_2024_Left_Rotate_Matrix_K_times.cpp

---
+++ 25_06_2024_Left_Rotate_Matrix_K_times.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    vector<vector<int>> rotateMatrix(int k, vector<vector<int>> mat) {
+        int n=mat.size(), m=mat[0].size();
+        vector<vector<int>>ans(n, vector<int>(m,0));
+        k%=m;
+        if(k==0) return mat;
+        for(int i=0;i<n;i++){
+            int x=0;
+            for(int j=k;j<m;j++,x++){
+                ans[i][x]=mat[i][j];
+            }
+            for(int j=0;j<k;j++,x++){
+                ans[i][x]=mat[i][j];
+            }
+        }
+        return ans;
+    }
+};
