a9e4fcaa8b01a2e3eddeef750a6e67dd82d3393c
AlgoZenithNITC <147307721+AlgoZenithNITC@users.noreply.github.com>
2024-07-03 18:14:00 +0530

    Create 03-07-2024_Remove_All_Occurences_of_Duplicates_in_a_Linked_List.cpp

---
+++ 03-07-2024_Remove_All_Occurences_of_Duplicates_in_a_Linked_List.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    Node* removeAllDuplicates(struct Node* head) {
+        Node* dummy = new Node(-1);
+        dummy->next = head;
+        
+        Node* prev = dummy;
+        Node* curr = head;
+        
+        while(curr){
+            while(curr->next && prev->next->data == curr->next->data){
+                curr = curr->next;
+            }
+            if(prev->next == curr){
+                prev = prev->next;
+            }
+            else{
+                prev->next = curr->next;
+            }
+            curr = curr->next;
+        }
+        head = dummy->next;
+        return head;
+    }
+};
