e2e45255d81f4f47e14597a6b9a7152ba135e916
AlgoZenithNITC <147307721+AlgoZenithNITC@users.noreply.github.com>
2024-07-08 18:09:34 +0530

    Create 08-07-2024_Search_in_Rotated_Sorted_Array.java

---
+++ 08-07-2024_Search_in_Rotated_Sorted_Array.java
@@ -0,0 +1,33 @@
+class Solution {
+    // Function to search for a key in a rotated sorted array.
+    int search(int[] arr, int key) {
+        int n = arr.length;
+        int start = 0, end = n - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            // If middle element is the key
+            if (arr[mid] == key) return mid;
+
+            // If the left part is sorted
+            if (arr[start] <= arr[mid]) {
+                if (arr[start] <= key && key <= arr[mid]) {
+                    // key is in the left part
+                    end = mid - 1;
+                } else {
+                    // key is in the right part
+                    start = mid + 1;
+                }
+            } else { // If the right part is sorted
+                if (arr[mid] <= key && key <= arr[end]) {
+                    // key is in the right part
+                    start = mid + 1;
+                } else {
+                    // key is in the left part
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
