a8c55738729660d95cfb633e6e4dd7e4952ddaad
AlgoZenithNITC <147307721+AlgoZenithNITC@users.noreply.github.com>
2024-07-21 15:52:06 +0530

    Create 21-07-2024-maximum-product-subset-of-an-array.java

---
+++ 21-07-2024-maximum-product-subset-of-an-array.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Solution {
+    public long findMaxProduct(int[] nums) {
+        int size = nums.length;
+        if (size == 1) {
+            return nums[0];
+        }
+
+        long result = 1;
+        int mod = 1000000007;
+        int minNegIndex = -1, minNegValue = 0;
+        int zeroCount = 0, negCount = 0;
+
+        for (int i = 0; i < size; i++) {
+            if (nums[i] == 0) {
+                zeroCount++;
+            } else if (nums[i] < 0) {
+                negCount++;
+                if (minNegIndex == -1 || nums[i] > minNegValue) {
+                    minNegIndex = i;
+                    minNegValue = nums[i];
+                }
+            }
+        }
+
+        if (zeroCount == size) {
+            return 0;
+        }
+
+        if (negCount == 1 && zeroCount == size - 1) {
+            return 0;
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (nums[i] == 0) {
+                continue;
+            }
+            if (negCount % 2 == 1 && i == minNegIndex) {
+                continue;
+            }
+            result = ((result * nums[i]) % mod + mod) % mod;
+        }
+        return result;
+    }
+}
