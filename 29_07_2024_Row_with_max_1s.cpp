14d3919400212db028c343de8f13f555ac700796
AlgoZenithNITC <147307721+AlgoZenithNITC@users.noreply.github.com>
2024-07-29 17:42:59 +0530

    Create 29_07_2024_Row_with_max_1s.cpp

---
+++ 29_07_2024_Row_with_max_1s.cpp
@@ -0,0 +1,24 @@
+//Back-end complete function template for C++
+
+class Solution {
+  public:
+    int rowWithMax1s(vector<vector<int> > &arr) {
+        int n = arr.size();
+        int m = arr[0].size();
+        int r = 0;              // keeps track of row; starts at first row
+        int c = m - 1;          // keeps track of column; starts at last column
+        int max_row_index = -1; // keeps track of result row index
+
+        // starting from top right corner
+        // go left if you encounter 1
+        // do down if you encounter 0
+        while (r < n && c >= 0) {
+            if (arr[r][c] == 1) {
+                max_row_index = r;
+                c--;
+            } else
+                r++;
+        }
+        return max_row_index;
+    }
+};
