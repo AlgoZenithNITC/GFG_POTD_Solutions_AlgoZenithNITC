de25ebf5390a1aa6845ae4db1f5fb223dcd56360
AlgoZenithNITC <147307721+AlgoZenithNITC@users.noreply.github.com>
2024-12-05 19:42:06 +0530

    Create 05-12-2024_Sort_0s_1s_and_2s.py

---
+++ 05-12-2024_Sort_0s_1s_and_2s.py
@@ -0,0 +1,13 @@
+def sort012(self, arr):
+        low, mid, high = 0, 0, len(arr) - 1
+
+        while mid <= high:
+            if arr[mid] == 0:
+                arr[low], arr[mid] = arr[mid], arr[low]
+                low += 1
+                mid += 1
+            elif arr[mid] == 1:
+                mid += 1
+            else:  # arr[mid] == 2
+                arr[mid], arr[high] = arr[high], arr[mid]
+                high -= 1
