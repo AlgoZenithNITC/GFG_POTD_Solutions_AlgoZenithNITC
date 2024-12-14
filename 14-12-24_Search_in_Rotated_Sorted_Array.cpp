00f32a544782342035550dda94eed36c92b6e0c5
AlgoZenithNITC <147307721+AlgoZenithNITC@users.noreply.github.com>
2024-12-14 19:34:21 +0530

    Create 14-12-24_Search_in_Rotated_Sorted_Array.cpp

---
+++ 14-12-24_Search_in_Rotated_Sorted_Array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    int search(vector<int>& arr, int key) {
+        int l=0,r=arr.size()-1;
+        while(l <= r){
+            int m = (l+r)/2;
+            if(arr[m]==key)
+                return m;
+            if(arr[m] > key){
+                if(arr[l] > key && arr[l] <= arr[m])
+                    l = m+1;
+                else
+                    r = m-1;
+            }else{
+                if(arr[r] < key && arr[m] <= arr[r])
+                    r = m-1;
+                else
+                    l = m+1;
+            }
+        }
+        return -1;
+    }
+};
