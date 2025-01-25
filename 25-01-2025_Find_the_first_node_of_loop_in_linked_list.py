bd82fd5513c0f0c7ae20590a36baa19e2cdfebec
AlgoZenithNITC <147307721+AlgoZenithNITC@users.noreply.github.com>
2025-01-25 20:43:18 +0530

    Create 25-01-2025_Find_the_first_node_of_loop_in_linked_list.py

---
+++ 25-01-2025_Find_the_first_node_of_loop_in_linked_list.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findFirstNode(self, head):
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                slow = head
+                while slow != fast:
+                    slow = slow.next
+                    fast = fast.next
+                return slow
+        return None
