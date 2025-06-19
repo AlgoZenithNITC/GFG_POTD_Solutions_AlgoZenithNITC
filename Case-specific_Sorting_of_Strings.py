75f192aab493d8886dd23dca17fb209c38e6a703
AlgoZenithNITC <147307721+AlgoZenithNITC@users.noreply.github.com>
2025-06-19 12:50:01 +0400

    Case-specific_Sorting_of_Strings.py

---
+++ Case-specific_Sorting_of_Strings.py
@@ -0,0 +1,23 @@
+class Solution:
+    def caseSort(self,s):
+        #code here
+        l=list(s)
+        l.sort()
+        t=""
+        i=0
+        while (i<len(l)):
+            if (l[i].islower()):
+                break
+            i=i+1
+        j=0
+        k=0
+        while (k<len(s)):
+            if (s[k].islower()):
+                t=t+l[i]
+                i=i+1
+            else:
+                t=t+l[j]
+                j=j+1
+            k+=1
+        
+        return t
