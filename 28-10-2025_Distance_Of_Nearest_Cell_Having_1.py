dbd51f8420db6c3f9149b11fbff70421db0e5a32
AlgoZenithNITC <147307721+AlgoZenithNITC@users.noreply.github.com>
2025-10-28 21:37:32 +0530

    Create 28-10-2025_Distance_Of_Nearest_Cell_Having_1.py

---
+++ 28-10-2025_Distance_Of_Nearest_Cell_Having_1.py
@@ -0,0 +1,24 @@
+def nearest(grid):
+    n = len(grid)
+    m = len(grid[0])
+
+    ans = [[float('inf')] * m for _ in range(n)]
+
+    # visit each cell of the grid
+    for i in range(n):
+        for j in range(m):
+
+            # if the cell has 1,
+            # then the distance is 0
+            if grid[i][j] == 1:
+                ans[i][j] = 0
+                continue
+
+            # iterate over all the cells
+            # and find the distance of the nearest 1
+            for k in range(n):
+                for l in range(m):
+                    if grid[k][l] == 1:
+                        ans[i][j] = min(ans[i][j],
+                                        abs(i - k) + abs(j - l))
+    return ans
