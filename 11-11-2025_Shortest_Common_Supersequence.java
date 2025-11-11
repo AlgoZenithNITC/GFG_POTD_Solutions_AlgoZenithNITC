5303dc10b8b3129df395c8558edc1ad02f2c3a0d
AlgoZenithNITC <147307721+AlgoZenithNITC@users.noreply.github.com>
2025-11-11 12:03:29 +0530

    11-11-2025_Shortest_Common_Supersequence.java

---
+++ 11-11-2025_Shortest_Common_Supersequence.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minSuperSeq(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1))
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                else
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+
+        return n + m - dp[n][m];
+    }
+}
